9a5a35ecb40c1a92478c116726960585edac88f3
Tom Wallis <thomaswallis0@gmail.com>
2025-02-22 20:26:50 -0500

    Only use falling edge as it's more reliable

--- read_button.py
+++ read_button.py
@@ -16,28 +16,22 @@ def edge_type_str(event):
 def watch_line_value(chip_path, offset):
     chip = gpiod.Chip(chip_path)
     line = chip.get_line(offset)
-    line.request("test", gpiod.LINE_REQ_EV_BOTH_EDGES, gpiod.LINE_REQ_FLAG_BIAS_PULL_UP)
-    debounce_ms = timedelta(milliseconds=250)
-    current_edge = None
+    line.request("test", gpiod.LINE_REQ_EV_FALLING_EDGE, gpiod.LINE_REQ_FLAG_BIAS_PULL_UP)
+    debounce_ms = timedelta(milliseconds=150)
     last_change = datetime.now()
     while True:
         event = line.event_read()
         now = datetime.now()
-        # print(now, edge_type_str(event))
-        if current_edge != event.type:
-            db_time = now - last_change
-            # print(f"{current_edge} -> {event.type}: {db_time}")
-            if db_time >= debounce_ms:
-                print("\r\n", db_time, " changed:", edge_type_str(event), "\r\n")
-            last_change = now
-            current_edge = event.type
+        db_time = now - last_change
+        if db_time >= debounce_ms:
+            print(db_time, " changed:", edge_type_str(event))
+        last_change = now
             
 
 if __name__ == "__main__":
     done_fd = os.eventfd(0)
 
-    watch_line_value("/dev/gpiochip0", 16)
-    # try:
-    #     watch_line_value("/dev/gpiochip0", 16)
-    # except OSError as ex:
-    #     print(ex, "\nCustomise the example configuration to suit your situation")
+    try:
+        watch_line_value("/dev/gpiochip0", 16)
+    except OSError as ex:
+        print(ex, "\nCustomise the example configuration to suit your situation")
